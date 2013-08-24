7f315ec0a2168f793006ce6f10196ce4f7a9f217
takumi <e.takumi.89@gmail.com>
2013-08-24 22:31:59 +0900

    changed color,

--- Resources/app.js
+++ Resources/app.js
@@ -9,6 +9,7 @@
 	require('lib/vineAPI').vineLogin('dreams.come.true.about@gmail.com','about19920429');
 	// require('lib/vineAPI').vineLogin('e.takumi.89@gmail.com','8Gatu9Ka');
 	
+	
 	Ti.App.disableNetworkActivityIndicator = true; //ネットワークインジゲータを消す
 	
 	tab_group = Ti.UI.createTabGroup();
@@ -29,7 +30,7 @@
 	var dummyWindow = [];
 	
 	//この個数は変動
-	for(var i=0;i<3;i++){
+	for(var i=0;i<4;i++){
 		var w = Ti.UI.createWindow({
 			width:"100%",
 			height:Ti.UI.FILL,
@@ -62,12 +63,13 @@
 			color : "#ffffff",
 			backgroundImage : image,
 			left:size,
-			width:"20px",
-		    height:"20px",
+			width:20,
+		    height:20,
 		});
 		return button;
 	};
 	
+		var homeButton = createImages("images/uniF489_.png",17);
 		var listButton = createImages("images/list.png",17);
 		var cameraButton = createImages("images/videocamera.png",153);
 		var searchButton = createImages("images/search.png",285);
@@ -77,54 +79,74 @@
 			width:"100%",
 			height:"10%",
 			bottom:0,
-			backgroundColor:"#00ccff",
+			backgroundColor:"#00a478",
 			color:"#fff",
 			textAlign:'center'
 		});
 	
+		//buttonBar.add(homeButton);
 		buttonBar.add(listButton);
 		buttonBar.add(cameraButton);
 		buttonBar.add(searchButton);
 
 // Butttonによる　画面遷移　未実装
 		Ti.App.createButton.addEventListener('singletap',function(e){
-			
 			//
-
+		});
+// 
+		var homebuttonBar = Ti.UI.createView({
+			width:"20%",
+			height:"100%",
+			top:0,
+			left:0,
+			image:"images/uniF489.png",
+			bubbleParent:false
 		});
 
+		// homebuttonBar.add(homeButton);
 
 	//上部ラベル
-		var topLabel = Ti.UI.createLabel({
+		var topLabel = Ti.UI.createLabel({//Label
 			width:"100%",
 			height:"10%",
 			top:0,
-			backgroundColor:"#00ccff",
-			text:'TL',
+			backgroundColor:"#00a478",
+			text:"Time Line",
+			font:{
+				fontWeight: "normal", 
+				fontSize: 25, 
+				fontFamily: 'AppleGothic'
+			},
 			color:"#fff",
-			textAlign:'center'
+			textAlign:'center',
+			bubbleParent:false
 		});
 	
+		topLabel.add(homebuttonBar);
+		homebuttonBar.add(homeButton);
+		
+		homebuttonBar.addEventListener('singletap',function(e){
+			console.log("ok");
+			//Ti.UI.ScrollableView. scrollToView(dummyWindow = [0]);
+		});
+		
+		topLabel.addEventListener('singletap',function(e){
+		});
+		
 	///////////////////////////////////////////////
 	//Labelとscrollableview
 		var topLabelNameArray = [];
 			topLabelNameArray[0]= "Time Line";
 			topLabelNameArray[1]= "Take Movie";
-			topLabelNameArray[2]= "Tags";
-			topLabelNameArray.font = {
-				fontSize : 17,
-				fontFamily : 'AppleGothic'
-			};
-		
-		// font: { fontSize: 17, fontFamily: 'AppleGothic', } ,
-		
+			topLabelNameArray[2]= "Best Vines";
+			topLabelNameArray[3]= "Profile";
+			
 		Ti.App.scrollview.addEventListener('scroll',function(e){
 			if(e.currentPage != undefined){
 				topLabel.text = topLabelNameArray[e.currentPage];	
 			}
 		});
 		
-	
 	///////////////////////////////////////////////
 	
 // 	
@@ -137,6 +159,11 @@
 		console.log('login');
 		require('UI/myTimeline').myTimeline();
 		// scrollview.views[0].add(require('UI/myTimeline').myTimeline());
+		//test
+		require('UI/myProfile').myProfile();
+		require('lib/vineAPI').myProfile();
+		//require('lib/vineAPI').tag();
+		
 	});
 	
 })();
