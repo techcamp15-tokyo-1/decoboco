db1697a752473593fe66f6da55809da1132d4e75
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2013-08-27 14:05:22 +0900

    no change

--- Resources/app.js
+++ Resources/app.js
@@ -3,6 +3,9 @@
 
 (function(){
 	
+	///////////
+	// require('lib/vineAPI').vineNewLogin('e.takumi.89@gmail.com','techcamptech','TechcampTakumi');
+	require('lib/vineAPI').vineNewLogin('higezizii-tansoku-choko-about@about-hiroppy.com','about19920429','about_hiroppy_test');
 	// var b = require('com.HjAboutHiroppy.VineCamera');
 	// var v = b.createView();
 	// console.log(v);
@@ -39,7 +42,7 @@
 			borderColor:"#fff000"
 		});
 		dummyWindow.push(w);	
-		}
+	}
 
 	//スクロールビューの挿入
 	Ti.App.scrollview = Ti.UI.createScrollableView({
@@ -55,7 +58,7 @@
         // pagingControlHeight: 30,
 	});
 	
-	  win_base.add(Ti.App.scrollview);
+	win_base.add(Ti.App.scrollview);
 	
 	
 	//Button の　追加
@@ -70,109 +73,105 @@
 		return button;
 	};
 	
-		var homeButton = createImages("images/uniF489_.png",17);
-		var listButton = createImages("images/list.png",17);
-		var cameraButton = createImages("images/videocamera.png",153);
-		var searchButton = createImages("images/search.png",285);
-		
-		//可視化用
-		var buttonBar = Ti.UI.createView({
-			width:"100%",
-			height:"10%",
-			bottom:0,
-			backgroundColor:"#00a478",
-			color:"#fff",
-			textAlign:'center'
-		});
+	var homeButton = createImages("images/uniF489_.png",17);
+	var listButton = createImages("images/list.png",17);
+	var cameraButton = createImages("images/videocamera.png",153);
+	var searchButton = createImages("images/search.png",285);
 	
-		//buttonBar.add(homeButton);
-		buttonBar.add(listButton);
-		buttonBar.add(cameraButton);
-		buttonBar.add(searchButton);
+	//可視化用
+	var buttonBar = Ti.UI.createView({
+		width:"100%",
+		height:"10%",
+		bottom:0,
+		backgroundColor:"#00a478",
+		color:"#fff",
+		textAlign:'center'
+	});
+	
+	//buttonBar.add(homeButton);
+	buttonBar.add(listButton);
+	buttonBar.add(cameraButton);
+	buttonBar.add(searchButton);
 
-// Butttonによる　画面遷移　未実装
-		Ti.App.createButton.addEventListener('singletap',function(e){
-			//
-		});
-// 
-		var homebuttonBar = Ti.UI.createView({
-			width:"20%",
-			height:"100%",
-			top:0,
-			left:0,
-			image:"images/uniF489.png",
-			bubbleParent:false
-		});
+    // Butttonによる　画面遷移　未実装
+	Ti.App.createButton.addEventListener('singletap',function(e){
+		//
+	});
+    // 
+	var homebuttonBar = Ti.UI.createView({
+		width:"20%",
+		height:"100%",
+		top:0,
+		left:0,
+		image:"images/uniF489.png",
+		bubbleParent:false
+	});
 
-		// homebuttonBar.add(homeButton);
+	// homebuttonBar.add(homeButton);
 
 	//上部ラベル
-		var topLabel = Ti.UI.createLabel({//Label
-			width:"100%",
-			height:"10%",
-			top:0,
-			backgroundColor:"#00a478",
-			text:"Time Line",
-			font:{
-				fontWeight: "normal", 
-				fontSize: 25, 
-				fontFamily: 'AppleGothic'
-			},
-			color:"#fff",
-			textAlign:'center',
-			bubbleParent:false
-		});
+	var topLabel = Ti.UI.createLabel({//Label
+		width:"100%",
+		height:"10%",
+		top:0,
+		backgroundColor:"#00a478",
+		text:"Time Line",
+		font:{
+			fontWeight: "normal", 
+			fontSize: 25, 
+			fontFamily: 'AppleGothic'
+		},
+		color:"#fff",
+		textAlign:'center',
+		bubbleParent:false
+	});
 	
-		topLabel.add(homebuttonBar);
-		homebuttonBar.add(homeButton);
-		
-		homebuttonBar.addEventListener('singletap',function(e){
-			console.log("ok");
-			//Ti.UI.ScrollableView. scrollToView(dummyWindow = [0]);
-		});
-		
-		topLabel.addEventListener('singletap',function(e){
-		});
-		
+	topLabel.add(homebuttonBar);
+	homebuttonBar.add(homeButton);
+	
+	homebuttonBar.addEventListener('singletap',function(e){
+		console.log("ok");
+		//Ti.UI.ScrollableView. scrollToView(dummyWindow = [0]);
+	});
+	
+	topLabel.addEventListener('singletap',function(e){
+	});
+	
 	///////////////////////////////////////////////
 	//Labelとscrollableview
-		var topLabelNameArray = [];
-			topLabelNameArray[0]= "Time Line";
-			topLabelNameArray[1]= "Take Movie";
-			topLabelNameArray[2]= "Best Vines";
-			topLabelNameArray[3]= "Profile";
-			
-		Ti.App.scrollview.addEventListener('scroll',function(e){
-			if(e.currentPage != undefined){
-				topLabel.text = topLabelNameArray[e.currentPage];	
-			}
-		});
-		
+	var topLabelNameArray = [];
+	topLabelNameArray[0]= "Time Line";
+	topLabelNameArray[1]= "Take Movie";
+	topLabelNameArray[2]= "Best Vines";
+	topLabelNameArray[3]= "Profile";
+	
+	Ti.App.scrollview.addEventListener('scroll',function(e){
+		if(e.currentPage != undefined){
+			topLabel.text = topLabelNameArray[e.currentPage];	
+		}
+	});
+	
 	///////////////////////////////////////////////
 	
-// 	
-		win_base.add(buttonBar);
-	    win_base.add(topLabel);
+    // 	
+	win_base.add(buttonBar);
+	win_base.add(topLabel);
 	// require('lib/camera').camera();
 	//これに入ったら各処理させる
 	Ti.App.addEventListener('loginComplete',function(){
 		console.log('login');
 		
-		require('lib/vineAPI').vinePopularTimeLine();
+		// require('lib/vineAPI').vinePopularTimeLine();
 		
 		// Ti.App.scrollview.views[0].add(require('UI/myTimeline').myTimeline());
 		//test
 		//require('UI/newLogin').newLogin();
-		require('UI/myProfile').myProfile();
-		 require('lib/vineAPI').myProfile();
+		// require('UI/myProfile').myProfile();
+		// require('lib/vineAPI').myProfile();
 		
+		require('lib/vineAPI').vineGraphTimeLine();
 		//require('lib/vineAPI').userData();
 		
 	});
-	
-	// Ti.App.addEventListener('userTimeLineComplete',function(e){
-		// console.log("---------------------------------------");
-		// console.log(e);
-	// });
 	
 })();
