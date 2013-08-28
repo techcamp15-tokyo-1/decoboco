adcc675d25321ae3b60e42b169fe24c93b295c8a
takumi <e.takumi.89@gmail.com>
2013-08-28 09:50:10 +0900

    no changed

--- Resources/UI/explore.js
+++ Resources/UI/explore.js
@@ -6,88 +6,76 @@ exports.explore = function(){
 		backgroundColor:"#ffffff"
 	});
 	
-var channelData = [
-			{
-		    title:"Comedy",
-		    hasChild:true,
-		    dest:"come"
-			},
-			
-			{
-		    title:"Art&Experimental",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Cats",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Dogs",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Family",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Beauty & Fashion",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Health & Fitness",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Nature",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Music",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"News & Politics",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Special FX",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Sports",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"Urban",
-		    hasChild:true,
-		    dest:""
-			},
-			
-			{
-		    title:"wier d",
-		    hasChild:true,
-		    dest:""
-			},
-];
+		var channelData = [
+					{
+				    title:"Comedy",
+				    hasChild:true,
+					},
+					
+					{
+				    title:"Art & Experimental",
+				    hasChild:true,
+					},
+					
+					{
+				    title:"Cats",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"Dogs",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"Family",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"Beauty & Fashion",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"Health & Fitness",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"Nature",
+				    hasChild:true,		    
+					},
+					
+					{
+					title:"Music",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"News & Politics",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"Special FX",
+				    hasChild:true,    
+					},
+					
+					{
+				    title:"Sports",
+				    hasChild:true,  
+					},
+					
+					{
+				    title:"Urban",
+				    hasChild:true,		    
+					},
+					
+					{
+				    title:"wier d",
+				    hasChild:true,
+					},
+		];
+
+};
