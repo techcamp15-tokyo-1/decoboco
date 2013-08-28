21997a9de3e62ff5f2a1571433e1c067186696c0
abouthiroppy <higezizii-tansoku-choko-about@about-hiroppy.com>
2013-08-28 21:07:46 +0900

    retain error

--- Resources/lib/regex.js
+++ Resources/lib/regex.js
@@ -1,9 +1,18 @@
 exports.prepareParse = function(json){
 		var str;
 		// str = json.replace(/([0-9])+[,]/g,"\""+"$&"+"\"");
-		str = json.replace(/([0-9])+[,]/g,function(num){
-			num = num.split(",");
-			return "\""+num[0]+"\",";
+		str = json.replace(/:\s([0-9]+)/g,function(num){
+			// console.log(num);
+			if(num.indexOf('.') != -1){
+				console.log('inin');
+				console.log(num);
+			}
+			num = num.replace(': ','');
+			num = num.replace(',', '');
+			num = num.replace(' ','');
+			
+			// console.log(num);
+			return ": \""+num+"\"";
 		});
 		return str;
 };
