04cd9f5952bb700dafb36e72a6cf20f43a7a4281
takumi <e.takumi.89@gmail.com>
2013-08-30 02:42:16 +0900

    new

---
+++ Resources/UI/setProfile.js
@@ -0,0 +1,11 @@
+exports.setProfile = function(){
+	
+		var set = Ti.UI.createView({
+			width:Ti.UI.FILL,
+			height:Ti.UI.FILL,
+			layout:"vertical",
+			top:0,
+			color:"#ffffff",
+		});
+		
+};
